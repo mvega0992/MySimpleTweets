04e6ca35bec518f858c63a6a19c91cf72efa448b
Miguel Vega <mvega@fb.com>
2018-07-06 16:56:51 -0700

    Styled tweet details activity

--- app/src/main/java/com/codepath/apps/restclienttemplate/DetailsActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/DetailsActivity.java
@@ -1,8 +1,9 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.format.DateUtils;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,10 +11,6 @@ import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 
 import org.parceler.Parcels;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -41,36 +38,11 @@ public class DetailsActivity extends AppCompatActivity {
         tvUsername.setText(tweet.user.name);
         tvBody.setText(tweet.body);
         tvScreenname.setText("@" + tweet.user.screenName);
-        tvRelativeTime.setText(getRelativeTimeAgo(tweet.createdAt));
+        tvRelativeTime.setText(tweet.createdAt);
         Glide.with(this).load(tweet.user.profileImageUrl).into(ivProfileImage);
     }
 
-    public String getRelativeTimeAgo(String rawJsonDate) {
-        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
-        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
-        sf.setLenient(true);
-
-        String relativeDate = "";
-
-        try {
-
-            long dateMillis = sf.parse(rawJsonDate).getTime();
-            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
-                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
-            String[] dateInfo = relativeDate.split(" ");
-            String amountTime = dateInfo[0];
-            String periodTime = dateInfo[1];
-
-            if (periodTime.contains("min")) relativeDate = amountTime + "m";
-            else if (periodTime.contains("hour")) relativeDate = amountTime + "h";
-            else if (periodTime.contains("year")) relativeDate = amountTime + "y";
-            else if (periodTime.contains("second")) relativeDate = amountTime + "s";
-            else relativeDate = "0s";
-
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        return relativeDate;
+    public void goBack(View v){
+        startActivity(new Intent(DetailsActivity.this, TimelineActivity.class));
     }
 }
